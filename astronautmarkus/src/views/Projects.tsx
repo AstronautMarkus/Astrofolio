be5a5a8cd89349e0d217cd38145305b180121ec8
Marcos Reyes <astronautmarkus@gmail.com>
2024-12-18 16:38:02 -0300

    feat: refactor Projects, Resume, and Stats views for improved layout and styling

--- astronautmarkus/src/views/Projects.tsx
+++ astronautmarkus/src/views/Projects.tsx
@@ -2,26 +2,28 @@ import './Home.css';
 
 const Home = () => (
   <div className="home-container d-flex justify-content-center align-items-center vh-100">
-    <div className="content-box shadow">
-      <h1 className="title">📂 My Projects</h1>
-      <div className="content">
-        <p>
-          Here are some of the projects I've been working on:
-        </p>
-        <ul className="project-list">
-          <li className="project-item">
-            <h2 className="project-title">Project 1</h2>
-            <p className="project-description">Description of project 1.</p>
-          </li>
-          <li className="project-item">
-            <h2 className="project-title">Project 2</h2>
-            <p className="project-description">Description of project 2.</p>
-          </li>
-          <li className="project-item">
-            <h2 className="project-title">Project 3</h2>
-            <p className="project-description">Description of project 3.</p>
-          </li>
-        </ul>
+    <div className="square-container">
+      <div className="content-box shadow">
+        <h1 className="title">📂 My Projects</h1>
+        <div className="content">
+          <p>
+            Here are some of the projects I've been working on:
+          </p>
+          <ul className="project-list">
+            <li className="project-item">
+              <h2 className="project-title">Project 1</h2>
+              <p className="project-description">Description of project 1.</p>
+            </li>
+            <li className="project-item">
+              <h2 className="project-title">Project 2</h2>
+              <p className="project-description">Description of project 2.</p>
+            </li>
+            <li className="project-item">
+              <h2 className="project-title">Project 3</h2>
+              <p className="project-description">Description of project 3.</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
--- astronautmarkus/src/views/Stats.tsx
+++ astronautmarkus/src/views/Stats.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Stats = () => (
-  <div className="content-section">
-    📊 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  <div className="home-container d-flex justify-content-center align-items-center vh-100">
+    <div className="square-container">
+      <div className="content-box text-center">
+        <div className="content-section">
+          📄 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
   </div>
 );
 
