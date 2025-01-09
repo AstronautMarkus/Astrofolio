f3c532def43744cc9d89951c3f1177d21afddaf8
astronautmarkus <astronautmarkus@gmail.com>
2025-01-08 21:46:54 -0300

    feat: add vertical centering to Home component for improved layout

--- astronautmarkus/src/Components/Containers/Home/Home.tsx
+++ astronautmarkus/src/Components/Containers/Home/Home.tsx
@@ -3,39 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRocket, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Home = ({ setSection, locale }: { setSection: (section: string) => void, locale: any }) => (
-  <div className="home-container d-flex justify-content-center align-items-center">
-    <div className="square-container">
-      <div className="content-box text-center">
-        <div className={`${styles.headerImage} mb-4 animate-fade-in`}>
-          <img
-            src="/assets/img/astronaut.png"
-            alt="Astronaut"
-            className={styles.astronautImage}
-          />
-        </div>
-        <h1 className={`${styles.title} text-white mb-3 mt-3`}>
-        🚀 {locale.welcome} <span className={styles.textGlowTitle}>{locale.myUniverse}</span>
-        </h1>
-        <p className="text-light">
-          {locale.introduction}
-        </p>
-        <p className="text-light">{locale.description}</p>
-        <p>{locale.ready}</p>
-        <div className="cta-buttons mt-4">
-          <button
-            onClick={() => setSection('projects')}
-            className={`${styles.purpleButton} me-3 btn-lg mb-3`}
-          >
-            <FontAwesomeIcon icon={faRocket} className="me-2" />
-            {locale.discoverProjects}
-          </button>
-          <button
-            onClick={() => setSection('contact')}
-            className={`${styles.purpleButton} btn-lg`}
-          >
-            <FontAwesomeIcon icon={faEnvelope} className="me-2" />
-            {locale.getInTouch}
-          </button>
+  <div className={`${styles.verticalCenter}`}>
+    <div className="home-container d-flex justify-content-center align-items-center">
+      <div className="square-container">
+        <div className="content-box text-center">
+          <div className={`${styles.headerImage} mb-4 animate-fade-in`}>
+            <img
+              src="/assets/img/astronaut.png"
+              alt="Astronaut"
+              className={styles.astronautImage}
+            />
+          </div>
+          <h1 className={`${styles.title} text-white mb-3 mt-3`}>
+          🚀 {locale.welcome} <span className={styles.textGlowTitle}>{locale.myUniverse}</span>
+          </h1>
+          <p className="text-light">
+            {locale.introduction}
+          </p>
+          <p className="text-light">{locale.description}</p>
+          <p>{locale.ready}</p>
+          <div className="cta-buttons mt-4">
+            <button
+              onClick={() => setSection('projects')}
+              className={`${styles.purpleButton} me-3 btn-lg mb-3`}
+            >
+              <FontAwesomeIcon icon={faRocket} className="me-2" />
+              {locale.discoverProjects}
+            </button>
+            <button
+              onClick={() => setSection('contact')}
+              className={`${styles.purpleButton} btn-lg`}
+            >
+              <FontAwesomeIcon icon={faEnvelope} className="me-2" />
+              {locale.getInTouch}
+            </button>
+          </div>
         </div>
       </div>
     </div>
