31d816efeacfdd74d4f7740f53f9b71d9b462408
astronautmarkus <astronautmarkus@gmail.com>
2025-01-09 21:24:27 -0300

    feat: update GeekZone component with new styling and content for improved user experience

--- astronautmarkus/src/Components/Containers/GeekZone/GeekZone.tsx
+++ astronautmarkus/src/Components/Containers/GeekZone/GeekZone.tsx
@@ -1,27 +1,25 @@
 import styles from './GeekZone.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 const GeekZone = ({ setSection, locale }: { setSection: (section: string) => void, locale: any }) => (
   <div className={`${styles.verticalCenter}`}>
     <div className="template-container d-flex justify-content-center align-items-center">
       <div className="square-container">
         <div className="content-box text-center">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <div className="cta-buttons mt-4">
+            <p>
+            This page is a work in progress. Please check back later ᗜ_ᗜ
+            </p>
+            <img src="/assets/img/fumo-fumo-fumo.gif" alt="Penguin" />
+            <div className="cta-buttons mt-4">
             <button
-              onClick={() => setSection('projects')}
+              onClick={() => setSection('home')}
               className={`${styles.purpleButton} me-3 btn-lg mb-3`}
             >
-              🚀 {locale.discoverProjects}
+              <FontAwesomeIcon icon={faHome} className="me-2" />
+              Home
             </button>
-            <button
-              onClick={() => setSection('contact')}
-              className={`${styles.purpleButton} btn-lg`}
-            >
-              ✉️ {locale.getInTouch}
-            </button>
-          </div>
+            </div>
         </div>
       </div>
     </div>
