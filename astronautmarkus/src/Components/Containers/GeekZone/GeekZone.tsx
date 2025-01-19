9401f85dd67871cbe1d5e1f08262e26ae2018588
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-18 21:25:15 -0300

    feat: enhance Geek Zone with new layout, carousel for playlists, and localization updates WIP

--- astronautmarkus/src/Components/Containers/GeekZone/GeekZone.tsx
+++ astronautmarkus/src/Components/Containers/GeekZone/GeekZone.tsx
@@ -7,19 +7,64 @@ const GeekZone = ({ setSection, locale }: { setSection: (section: string) => voi
     <div className="template-container d-flex justify-content-center align-items-center">
       <div className="square-container">
         <div className="content-box text-center">
-            <p>
-            {locale.geekZoneWip}
-            </p>
-            <img src="/assets/img/fumo-fumo-fumo.gif" alt="Penguin" />
-            <div className="cta-buttons mt-4">
-            <button
-              onClick={() => setSection('home')}
-              className={`${styles.purpleButton} me-3 btn-lg mb-3`}
-            >
-              <FontAwesomeIcon icon={faHome} className="me-2" />
-              {locale.home}
-            </button>
+            <h1 className='title text-center'>🤓 AstronautMarkus Geek Zone</h1>
+            <p className='text-center'>{locale.geekZoneDescription}</p>
+            
+
+            <div className="content">
+              <ul className="project-list">
+                <li className="project-item d-flex flex-column flex-md-row">
+                  <ul className='project-list'>
+                    <li>
+                      <div className="text-center">
+
+                        <p>
+                          {locale.geekZoneInfo}
+                        </p>
+                        
+                        <div className="project-image">
+                          <img src="/assets/img/geekzone/hi-aya-shameimaru.gif" className='rounded' alt="hi-aya-shameimaru" />
+                        </div>
+
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </div>
+
+            <div className="content playlist-carousel">
+              <h5 className='text-center'>{locale.geekZoneMusic}</h5>
+              <div id="playlistCarousel" className="mt-5 carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                <iframe style={{ borderRadius: '12px', width: '70%' }} src="https://open.spotify.com/embed/playlist/03OXOTX3VQyHLpQMb0lVwW?utm_source=generator" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
+                <div className="carousel-item">
+                <iframe style={{ borderRadius: '12px', width: '70%' }} src="https://open.spotify.com/embed/playlist/2XAXSFbOpDxa0FEFxoalLL?utm_source=generator" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
+                <div className="carousel-item">
+                <iframe style={{ borderRadius: '12px', width: '70%' }} src="https://open.spotify.com/embed/playlist/7JNAaUot7j1z9gYcRDW98D?utm_source=generator" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#playlistCarousel" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#playlistCarousel" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+              </div>
+            </div>
+
+            <div className="cta-buttons mt-4">
+              <button onClick={() => setSection('home')}className={`${styles.purpleButton} me-3 btn-lg mb-3`}>
+                <FontAwesomeIcon icon={faHome} className="me-2" />
+                {locale.home}
+              </button>
+            </div>
+
         </div>
       </div>
     </div>
